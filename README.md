de9a66c18015c8c44d2d83d7385abb64791a3ea2
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-01-05 09:30:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Elmish-fss-template
+
+A simple F# elmish template with:
+- F# 5
+- Fable 3
+- Paket
+- Fss
+- Elmish
+
+- How to run:
+```
+dotnet tool restore
+paket install
+npm install
+npm start
+```
+Go to localhost:1234
---
+++ src/App.fs
@@ -0,0 +1,50 @@
+﻿namespace Docs
+
+// Copy of the reactjs.org todo list.
+
+module App =
+    open Elmish
+    open Elmish.React
+    open Fable.React
+    open Fable.React.Props
+
+    type Model = {
+        Input: string
+        Todos: string list
+    }
+
+    type Msg =
+        | SetInput of string
+        | AddTodo of string
+
+    let init() = {
+        Input = ""
+        Todos = []
+    }
+
+    let update (msg: Msg) (model: Model): Model =
+        match msg with
+        | SetInput input -> { model with Input = input }
+        | AddTodo todo ->
+            { model with
+                Todos = todo::model.Todos
+                Input = "" }
+
+    let render (model: Model) (dispatch: Msg -> unit) =
+        div []
+            [
+                h3 [] [ str "TODO" ]
+                str "What needs to be done"
+                ul [] <| List.map (fun todo -> li [] [ str todo ]) model.Todos
+                input
+                    [
+                        Value model.Input
+                        OnChange (fun e -> e.Value |> SetInput |> dispatch)
+                    ]
+                button [ OnClick (fun _ -> model.Input |> AddTodo |> dispatch) ]
+                    [ str $"Add #{List.length model.Todos}" ]
+            ]
+
+    Program.mkSimple init update render
+    |> Program.withReactSynchronous "elmish-app"
+    |> Program.run
