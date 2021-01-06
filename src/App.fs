f931cfb7c7de35610770aedf9f86ae448adbe2b5
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-01-06 10:32:09 +0100

    Finish up quick example

--- src/App.fs
+++ src/App.fs
@@ -9,13 +9,6 @@ module App =
     open Fable.React.Props
     open Fss
 
-    // Colors
-    let blue = CSSColor.Hex "0d6efd"
-    let darkBlue = CSSColor.Hex "01398D"
-
-    // Fonts
-    let textFont = FontFamily.Custom "Roboto"
-
     type Model = {
         Input: string
         Todos: string list
@@ -39,98 +32,20 @@ module App =
                 Input = "" }
 
     let render (model: Model) (dispatch: Msg -> unit) =
-        let formStyle =
+        div []
             [
-                Display.InlineBlock
-                Padding.Value(px 10, px 15)
-                FontSize' (px 18);
-                BorderRadius' (px 0)
-            ]
-        let container =
-            fss
-                [
-                    Display.Flex
-                    FlexDirection.Column
-                    Padding.Value(rem 0., rem 1.5)
-                    textFont
-                ]
-        let buttonStyle =
-            fss
-                [
-                    yield! formStyle
-                    Border.None
-                    BackgroundColor' blue
-                    Color.white
-                    Width' (em 10.)
-                    Hover
-                        [
-                            Cursor.Pointer
-                            BackgroundColor' darkBlue
-                        ]
-                ]
-        let inputStyle =
-            fss
-                [
-                    yield! formStyle
-                    BorderRadius' (px 0)
-                    BorderWidth.Thin
-                    MarginRight' (px 25)
-                    Width' (px 400)
-                ]
-        let header =
-            fss
-                [
-                    textFont
-                    Color' blue
-                ]
-        let todoStyle =
-            let fadeInAnimation =
-                keyframes
-                    [
-                        frame 0 
-                            [
-                                Opacity' 0.
-                                Transform.TranslateY(px 20)
-                            ]
-                        frame 100 
-                            [
-                                Opacity' 1.
-                                Transform.TranslateY(px 0)
-                            ]
-                    ]
-            let indexCounter = counterStyle []
-            fss
-                [
-                    CounterIncrement' indexCounter
-                    MarginTop' (px 1)
-                    Width.MaxContent
-                    FontSize' (px 20);
-                    AnimationName' fadeInAnimation
-                    AnimationDuration' (sec 0.4)
-                    AnimationTimingFunction.Ease
-                    Before
-                        [
-                            Color.Hex "48f"
-                            Content.Counter(indexCounter,". ")
-                        ]
-                ]
-
-        div [ ClassName container ]
-            [
-                h2 [ ClassName header ] [ str "TODO" ]
-                ul [] <| List.map (fun todo -> li [ ClassName todoStyle ] [ str todo ]) model.Todos
+                h2 [] [ str "TODO" ]
+                ul [] <| List.map (fun todo -> li [] [ str todo ]) model.Todos
                 div []
                     [
                         input
                             [
-                                ClassName inputStyle
                                 Placeholder "What needs to be done?"
                                 Value model.Input
                                 OnChange (fun e -> e.Value |> SetInput |> dispatch)
                             ]
                         button
                             [
-                                ClassName buttonStyle
                                 OnClick (fun _ -> model.Input |> AddTodo |> dispatch)
                             ]
                             [ str $"Add #{List.length model.Todos}" ]
