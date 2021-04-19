2cf0f057a18bf393d8ddc8fb01461a29f0b59046
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-04-19 14:57:05 +0200

    Update to parcel 2

--- src/App.fs
+++ src/App.fs
@@ -1,57 +1,55 @@
-﻿namespace Docs
+﻿// Copy of the reactjs.org todo list.
 
-// Copy of the reactjs.org todo list.
+module App
+open Elmish
+open Elmish.React
+open Fable.React
+open Fable.React.Props
+open Fss
 
-module App =
-    open Elmish
-    open Elmish.React
-    open Fable.React
-    open Fable.React.Props
-    open Fss
+type Model = {
+    Input: string
+    Todos: string list
+}
 
-    type Model = {
-        Input: string
-        Todos: string list
-    }
+type Msg =
+    | SetInput of string
+    | AddTodo of string
 
-    type Msg =
-        | SetInput of string
-        | AddTodo of string
+let init() = {
+    Input = ""
+    Todos = []
+}
 
-    let init() = {
-        Input = ""
-        Todos = []
-    }
+let update (msg: Msg) (model: Model): Model =
+    match msg with
+    | SetInput input -> { model with Input = input }
+    | AddTodo todo ->
+        { model with
+            Todos = model.Todos @ [todo]
+            Input = "" }
 
-    let update (msg: Msg) (model: Model): Model =
-        match msg with
-        | SetInput input -> { model with Input = input }
-        | AddTodo todo ->
-            { model with
-                Todos = model.Todos @ [todo]
-                Input = "" }
+let render (model: Model) (dispatch: Msg -> unit) =
+    div []
+        [
+            h2 [] [ str "TODO" ]
+            ul [] <| List.map (fun todo -> li [] [ str todo ]) model.Todos
+            div []
+                [
+                    input
+                        [
+                            Placeholder "What needs to be done?"
+                            Value model.Input
+                            OnChange (fun e -> e.Value |> SetInput |> dispatch)
+                        ]
+                    button
+                        [
+                            OnClick (fun _ -> model.Input |> AddTodo |> dispatch)
+                        ]
+                        [ str $"Add #{List.length model.Todos}" ]
+                ]
+        ]
 
-    let render (model: Model) (dispatch: Msg -> unit) =
-        div []
-            [
-                h2 [] [ str "TODO" ]
-                ul [] <| List.map (fun todo -> li [] [ str todo ]) model.Todos
-                div []
-                    [
-                        input
-                            [
-                                Placeholder "What needs to be done?"
-                                Value model.Input
-                                OnChange (fun e -> e.Value |> SetInput |> dispatch)
-                            ]
-                        button
-                            [
-                                OnClick (fun _ -> model.Input |> AddTodo |> dispatch)
-                            ]
-                            [ str $"Add #{List.length model.Todos}" ]
-                    ]
-            ]
-
-    Program.mkSimple init update render
-    |> Program.withReactSynchronous "elmish-app"
-    |> Program.run
+Program.mkSimple init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
